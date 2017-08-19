d535ebb1dfec558afdc28d7b495485b805b3b3c9
Curtis Schumacher <cschumacr@gmail.com>
2017-08-19 18:24:25 -0400

    remove auth from joingame

--- src/components/JoinGame/index.js
+++ src/components/JoinGame/index.js
@@ -1,20 +1,10 @@
 import React from 'react'
 import { compose, lifecycle } from 'recompose'
 
-import auth from '../../auth'
-
-const enhance = compose(
-  lifecycle({
-    componentWillMount() {
-      auth.signInAnonymously();
-    }
-  })
-);
-
 const JoinGame = () => (
   <div>
     MUSIC QUIZ 2.0
   </div>
 );
 
-export default enhance(JoinGame);
+export default JoinGame;
