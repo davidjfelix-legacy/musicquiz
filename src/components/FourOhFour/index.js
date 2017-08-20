eee2e5f431bb8e0213b37954bcd948e3429df81b
Curtis Schumacher <cschumacr@gmail.com>
2017-08-19 21:37:23 -0400

    404'

---
+++ src/components/FourOhFour/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Screen } from '../screen'
+
+const FourOhFour = () => (
+    <Screen> 
+        <h1 style={{fontSize: '4em', margin: '0 auto', marginTop: '45%'}} >404 :(</h1>
+    </Screen>
+)
+
+
+
+export default FourOhFour;
