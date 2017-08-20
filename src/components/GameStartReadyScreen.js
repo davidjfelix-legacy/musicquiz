556fbf6253ec963969fd186625c3095a831e28bb
Mark Chandler <mark.allen.chandler@gmail.com>
2017-08-20 03:14:17 -0400

    Add settings table to GameStartReadyScreen

--- src/components/GameStartReadyScreen.js
+++ src/components/GameStartReadyScreen.js
@@ -11,23 +11,38 @@ import './GameStartReadyScreen.css'
 
 const game = {
   code: 'A64E',
-  type: 'Automagic'
+  type: 'Automagic',
+  players: []
 }
 
 const GameStartReadyScreen = () => (
   <Screen className="GameStartReadyScreen">
     <ScreenHeader>{game.type} Game</ScreenHeader>
-    <ScreenBody backgroundColor="white">
+    <ScreenBody topBar bottomBar>
       <div className="GameStartReadyScreen-code">Game Code: {game.code}</div>
-      <div className="GameStartReadyScreen-settings">
-        <div className="title">Settings</div>
+      <div className="GameStartReadyScreen-Settings">
+        <div className="Settings-Title title">Settings</div>
+        <ul className="Settings-List">
+          <li className="List-Row">
+            <span className="Row-SettingName">Playlist</span>
+            <span className="Row-SettingValue">Viral Top 50</span>
+          </li>
+          <li className="List-Row">
+            <span className="Row-SettingName">Song Count</span>
+            <span className="Row-SettingValue">10</span>
+          </li>
+          <li className="List-Row">
+            <span className="Row-SettingName">Clip Duration</span>
+            <span className="Row-SettingValue">30 seconds</span>
+          </li>
+        </ul>
       </div>
       <div className="GameStartReadyScreen-players">
         <GameUsersList gameId="FIXME" />
       </div>
     </ScreenBody>
     <ScreenFooter>
-      <ExpandoButton href="#">Start</ExpandoButton>
+      <ExpandoButton disabled={!game.players.length > 0} href="#">Start</ExpandoButton>
     </ScreenFooter>
   </Screen>
 )
