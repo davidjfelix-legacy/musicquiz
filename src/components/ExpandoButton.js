d002a1435283461d67747f1dcb8fb2f877f4148c
Mark Chandler <mark.allen.chandler@gmail.com>
2017-08-20 03:13:32 -0400

    Add disabled state to ExpandoButton

--- src/components/ExpandoButton.js
+++ src/components/ExpandoButton.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import './ExpandoButton.css'
 
-const ExpandoButton = ({ children, href }) => (
-  <a className="ExpandoButton" href={href}>
-    <div className="ExpandoButton-content">
-      {children}
-    </div>
-  </a>
-)
+const ExpandoButton = ({ children, disabled, href }) => {
+  if (disabled) {
+    return (
+      <div className="ExpandoButton ExpandoButton-disabled">
+        <div className="ExpandoButton-content">
+          {children}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <a className="ExpandoButton" href={href}>
+      <div className="ExpandoButton-content">
+        {children}
+      </div>
+    </a>
+  )
+}
 
 export default ExpandoButton
