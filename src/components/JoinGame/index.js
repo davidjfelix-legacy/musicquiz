53a27c09ff5f90254145016234c94e4b79861581
Curtis Schumacher <cschumacr@gmail.com>
2017-08-20 02:12:35 -0400

    joingame

--- src/components/JoinGame/index.js
+++ src/components/JoinGame/index.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import { 
+  compose,
+  withState,
+  withHandlers
+} from 'recompose'
 
-const JoinGame = () => (
-  <div>
-    MUSIC QUIZ 2.0
-  </div>
+
+import {
+  ScreenBody,
+  Screen,
+  ScreenHeader,
+  ScreenFooter
+} from '../screen'
+
+import ExpandoButton from '../ExpandoButton'
+
+import './index.css'
+
+const enhance = compose(
+  withState('gameCode', 'setGameCode', ''),
+  withHandlers({
+    onGameCodeChange: props => event => {
+      console.log(event);
+      props.setGameCode(event.target.value);
+    }
+  })
 )
 
-export default JoinGame
+const JoinGame = ({gameCode, onGameCodeChange}) => (
+  <Screen>
+    <ScreenBody className={'JoinGame-container'}>
+      <div style={{margin: '0 auto', textAlign: 'center'}}>
+        <span className={'JoinGame-gameCodeLabel'}>Game Code:</span>
+        <input id='gameCode' name='gameCode' type='gameCode' value={gameCode} className={'JoinGame-gameCode'} onChange={onGameCodeChange}></input>
+      </div>
+
+      <div style={{margin: '0 auto', textAlign: 'center'}}>
+        <span className={'JoinGame-userNameLabel'}>User Name:</span>
+        <input id='gameCode' name='gameCode' type='gameCode' value={gameCode} className={'JoinGame-userName'} onChange={onGameCodeChange}></input>
+      </div>
+    </ScreenBody>
+    <ScreenFooter>
+      <ExpandoButton href='join'>
+        Join!
+      </ExpandoButton>
+    </ScreenFooter>
+  </Screen>
+);
+
+export default enhance(JoinGame)
