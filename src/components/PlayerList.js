5a631b0dbc025a2daefc0a21ebfb792f50e59201
Mark Chandler <mark.allen.chandler@gmail.com>
2017-08-20 01:57:35 -0400

    Add empty players list message to PlayerList

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PlayerListRow from './PlayerListRow'
 import UserColor from './UserColor'
 import UserName from './Username'
 import './PlayerList.css'
@@ -22,14 +23,16 @@ const PlayerList = ({ userIds }) => (
   <div className="PlayerList">
     <div className="title">Current Players</div>
     <ul className="PlayerList-list">
-      {userIds.map(userId =>
-        <li
-          className="PlayerList-player"
-        >
-          <UserColor userId={userId} />
-          <UserName userId={userId} />
-        </li>
-      )}
+      {userIds && userIds.length > 0
+        ? userIds.map(userId =>
+          <li
+            className="PlayerList-player"
+          >
+            <UserColor userId={userId} />
+            <UserName userId={userId} />
+          </li>)
+        : <div className="PlayerList-empty">Game currently has no players. Share the game code with your friends!</div>
+      }
     </ul>
   </div>
 )
